237f63c8aa35650622335dcbc91d91e94eeb6bba
andrescasa-dev <andrescasa.dev@gmail.com>
2022-03-04 16:51:51 -0500

    play button and juice funcitonalities were added

--- src/video-player.js
+++ src/video-player.js
@@ -1,3 +1,26 @@
-import { videoSources } from "./videos-data";
 const video = document.getElementById('video');
+const btn_play = document.getElementById('play-button');
+const juice = document.getElementById('juice');
+console.log("este es el script");
+
+function playAction(){
+  btn_play.classList.toggle('fa-play');
+  if(video.paused){
+    video.play();
+  }
+  else{
+    video.pause();
+    
+  }
+}
+
+function updateJuice(){
+  let currentTimePercent = (video.currentTime / video.duration) * 100;
+  juice.style.width = currentTimePercent + "%";
+}
+
+btn_play.addEventListener('click',playAction);
+
+video.addEventListener('timeupdate', updateJuice)
+
 
